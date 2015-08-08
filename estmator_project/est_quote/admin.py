33c83e388596481c902d1443eb4c9b5df759c4f9
Scott Schmidt <sjschmidt44@gmail.com>
2015-08-08 14:43:09 -0700

    update admin panel

--- estmator_project/est_quote/admin.py
+++ estmator_project/est_quote/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Product, Quote
+from .models import Product, Quote, Category
 
 admin.site.register(Product)
+admin.site.register(Category)
 admin.site.register(Quote)
