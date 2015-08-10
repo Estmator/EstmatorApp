34ac20ee580ea031baf172dd7ced82d1228c26f1
Jim Grant <jimgrant@gmail.com>
2015-08-10 10:36:10 -0700

    add quote page related static files, quote page functions

--- estmator_project/estmator_project/settings.py
+++ estmator_project/estmator_project/settings.py
@@ -21,6 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "estmator_project/static"),
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
