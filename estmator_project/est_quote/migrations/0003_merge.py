5f7c988feb2334a3615e4bcfbf2f076eefdf3dbb
Jim Grant <jimgrant@gmail.com>
2015-08-12 11:39:44 -0700

    merge jonathan's branch migration

---
+++ estmator_project/est_quote/migrations/0003_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('est_quote', '0002_auto_20150812_1825'),
+        ('est_quote', '0002_auto_20150811_2326'),
+    ]
+
+    operations = [
+    ]
