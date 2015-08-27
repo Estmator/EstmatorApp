1cf204d965146e285666dbcad9bc21e6dbfe0ebe
Jonathan Stallings <jonathanstallings@outlook.com>
2015-08-27 14:19:32 -0700

    Fix menu.js function declaration order

--- estmator_project/estmator_project/static/js/menu.js
+++ estmator_project/estmator_project/static/js/menu.js
@@ -8,11 +8,27 @@ $(function () {
         $('#edit_client_form').clearForm();
         $('#edit_client').modal('toggle');
     }
+    
+    function newCompanySuccess() {
+        $('#new_company_form').clearForm();
+        $('#new_company').modal('toggle');
+    }
+    function editCompanySuccess() {
+        $('#edit_company_form').clearForm();
+        $('#edit_company').modal('toggle');
+    }
 
     $('#new_client_form').ajaxForm({
         success: newClientSuccess
     });
     $('#edit_client_form').ajaxForm({
         success: editClientSuccess
+    });
+
+    $('#new_company_form').ajaxForm({
+        success: newCompanySuccess
+    });
+    $('#edit_company_form').ajaxForm({
+        success: editCompanySuccess
     });
 });
